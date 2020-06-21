5aebeec6d646b46d4c8320ecb8a88114eb44ee0f
George Mamar <george@mamar.dev>
2020-06-21 11:27:17 +0200

    save heights in node and update only on ops

--- main.hs
+++ main.hs
@@ -1,73 +1,82 @@
 module AVLTree where
 import Debug.Trace
 data Tree a = EmptyTree 
-            | Node a (Tree a) (Tree a) 
-            -- corresponds to (nodeContent) leftTree rightTree
+            | Node (a,Int) (Tree a) (Tree a) 
+            -- corresponds to (nodeContent, nodeHeight) leftTree rightTree
             deriving (Eq, Read)
 
 -- returns a balanced tree after inserting a given element
 insert :: (Ord a) => a -> Tree a -> Tree a
-insert x EmptyTree = Node x EmptyTree EmptyTree
-insert x (Node v l r)
-    | x < v = rebalance $ (Node v (insert x l) r)
-    | otherwise = rebalance $ (Node v l (insert x r))
+insert x EmptyTree = Node (x,1) EmptyTree EmptyTree
+insert x t@(Node (v,_) l r)
+    | x < v = rebalance $ (Node (v,hl) (insert x l) r)
+    | otherwise = rebalance $ (Node (v,hr) l (insert x r)) where
+        hl = (1 + max (height (insert x l)) (height r))
+        hr = (1 + max (height l) (height (insert x r))) 
 
--- deletes a given element if found, without rebalancing the tree
+-- deletes a given element if found
 delete :: (Ord a) => a -> Tree a -> Tree a 
 delete _ EmptyTree = EmptyTree
-delete x t@(Node v l r)
-    | x < v = rebalance (Node v (delete x l) r)
-    | x > v = rebalance (Node v l (delete x r))
+delete x t@(Node (v,h) l r)
+    | x < v = rebalance (Node (v,hl) (delete x l) r)
+    | x > v = rebalance (Node (v,hr) l (delete x r))
     | x == v && l == EmptyTree = r
     | x == v && r == EmptyTree = l
-    | otherwise = (Node succ (delete succ l) r) 
+    | otherwise = (Node (succ,1) (delete succ l) r) 
     where
-        succ = inOrderSucc t
+        hl = (1 + max (height (delete x l)) (height r))
+        hr = (1 + max (height l) (height (delete x r))) 
+        succ = inOrderSucc $ left t
         -- inOrderSucc is technically a partial function, but recursion handles EmptyTree beforehand
-        inOrderSucc :: (Ord a) => Tree a -> a 
-        inOrderSucc (Node v EmptyTree _) = v
-        inOrderSucc (Node _ l _)         = inOrderSucc l
+        inOrderSucc :: (Ord a) => Tree a -> a
+        inOrderSucc (Node (v,_) _ EmptyTree) = v
+        inOrderSucc (Node _ _ r)             = inOrderSucc r
 
 -- rebalances a tree
 rebalance :: Tree a -> Tree a
 rebalance EmptyTree = EmptyTree
-rebalance t@(Node v l r)
+rebalance t@(Node (v,h) l r)
     -- op didnt require rebalancing, balance factor is still in range
     | abs (diff t) < 2        = t
     -- new node was created left.left of root
     | diff t == 2 && dl /= -1 = rotateR t
     -- new node was created left.right of root
-    | diff t == 2 && dl == -1 = rotateR (Node v (rotateL l) r)
+    | diff t == 2 && dl == -1 = rotateR (Node (v,h) (rotateL l) r)
     -- new node was created right.right of root
     | diff t == -2 && dr /= 1 = rotateL t
     -- new node was created right.left of root
-    | diff t == -2 && dr == 1 = rotateL (Node v l (rotateR r))
+    | diff t == -2 && dr == 1 = rotateL (Node (v,h) l (rotateR r))
     -- tree wasnt an AVL Tree to begin with
     | otherwise = t
     where
         dl = diff l
         dr = diff r
-        -- helper to return the height value of a given tree
-        height :: Tree a -> Int
-        height EmptyTree = 0
-        height (Node _ l r) = 1 + max (height l) (height r)
 
         -- returns the height difference of a tree's children
         diff :: Tree a -> Int
         diff EmptyTree = 0
-        diff (Node v l r) = (height l) - (height r)
+        diff (Node _ l r) = (height l) - (height r)
 
         -- rotates a tree around its root to the right
         rotateR :: Tree a -> Tree a
         rotateR EmptyTree = EmptyTree
-        rotateR (Node x (Node y sl sr) r) = Node y sl (Node x sr r)
+        rotateR (Node (x,h) (Node (y,k) sl sr) r) = Node (y,k') sl (Node (x,h') sr r) where
+            h' = 1 + max (height sr) (height r)
+            k'  = 1 + max (height sl) h'
 
         -- rotates a tree around its root to the left
         rotateL :: Tree a -> Tree a
         rotateL EmptyTree = EmptyTree
-        rotateL (Node x l (Node y sl sr)) = Node y (Node x l sl) sr
+        rotateL (Node (x,h) l (Node (y,k) sl sr)) = Node (y,k') (Node (x,h') l sl) sr where
+            h' = 1 + max (height l) (height sl)
+            k' = 1 + max (height sr) h'
 
 -- Not part of the data structure implementation
+-- helper to return the height of a given tree's root
+height :: Tree a -> Int
+height EmptyTree = 0
+height (Node (_,h) l r) = h
+
 -- helper to return the left child of a tree
 left :: Tree a -> Tree a
 left EmptyTree = EmptyTree
@@ -87,16 +96,16 @@ instance (Show a) => Show (Tree a) where
     show t = "root \t -> \t leaf \n" ++ print t 0 where
         print :: (Show a) => (Tree a) -> Int -> String
         print EmptyTree _ = ""
-        print (Node v l r) space = right ++ "\n" ++ (emptyLoop v space) ++ left where
+        print (Node (v,b) l r) space = right ++ "\n" ++ (emptyLoop (v,b) space) ++ left where
             right = print r (space+6)
             left = print l (space+6)
-            emptyLoop v 0 = show v
-            emptyLoop v s = " " ++ emptyLoop v (s-1)
+            emptyLoop (v,b) 0 = show v ++ "," ++ show b
+            emptyLoop (v,b) s = " " ++ emptyLoop (v,b) (s-1)
 
 -- test trees
 tree1 :: Tree Int
-tree1 = Node 3 (Node 1 EmptyTree EmptyTree) (Node 5 ((Node 4 EmptyTree EmptyTree)) ((Node 8 ((Node 7 EmptyTree EmptyTree)) ((Node 15 EmptyTree EmptyTree))))) 
+tree1 = Node (3,4) (Node (1,1) EmptyTree EmptyTree) (Node (5,3) ((Node (4,1) EmptyTree EmptyTree)) ((Node (8,2) ((Node (7,1) EmptyTree EmptyTree)) ((Node (15,1) EmptyTree EmptyTree))))) 
 tree2 :: Tree Int
-tree2 = Node 3 (Node 1 EmptyTree (Node 2 EmptyTree EmptyTree)) (Node 5 ((Node 4 EmptyTree EmptyTree)) ((Node 8 ((Node 7 EmptyTree EmptyTree)) ((Node 15 EmptyTree EmptyTree))))) 
+tree2 = Node (3,4) (Node (1,2) EmptyTree (Node (2,1) EmptyTree EmptyTree)) (Node (5,3) ((Node (4,1) EmptyTree EmptyTree)) ((Node (8,2) ((Node (7,1) EmptyTree EmptyTree)) ((Node (15,1) EmptyTree EmptyTree)))))
 tree3 :: Tree Int
-tree3 = Node 3 (Node 1 EmptyTree EmptyTree) (Node 5 ((Node 4 EmptyTree EmptyTree)) ((Node 8 EmptyTree EmptyTree))) 
+tree3 = Node (3,3) (Node (1,1) EmptyTree EmptyTree) (Node (5,2) ((Node (4,1) EmptyTree EmptyTree)) ((Node (8,1) EmptyTree EmptyTree)))
